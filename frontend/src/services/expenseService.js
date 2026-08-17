92233266a5898f5094cee44802ba6e900a714522
Piyush Agrahari <128026941+Piiush04@users.noreply.github.com>
2026-08-17 16:43:59 +0530

    updating url

--- frontend/src/services/expenseService.js
+++ frontend/src/services/expenseService.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5000";
+const API_URL = "https://expense-tracker-ayup.onrender.com";
 
 export const addExpense = async(title, amount, category, expenseDate, token) => {
     try {
